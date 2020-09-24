6feb1863985fb693ae38b2f332b3ab15a9e17ba7
leojely <52141116+leojely@users.noreply.github.com>
2020-09-24 04:18:39 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'view/index.html'));
+});
+ 
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'view/contact.html'));
+});
+
+app.get('/thanks', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'view/thanks.html'));
+});
+ 
